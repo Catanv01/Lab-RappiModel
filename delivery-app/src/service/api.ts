851ddbecc36fc3914e82bcc266109bc0258ed66d
Catalina Nuñez Valencia <catalinanunez@MacBook-Air-de-Catalina-2.local>
2026-03-18 00:33:32 -0500

    first frontend website uploaded on vercel

--- delivery-app/src/service/api.ts
+++ delivery-app/src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:1234/api',
+  baseURL: 'https://lab-rappi-model.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
